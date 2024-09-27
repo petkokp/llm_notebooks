c5139f56dae3d2c1f3a82f002ebb65058d7d194b
Petko Petkov <petko.petkov987@gmail.com>
2024-09-27 20:55:23 +0200

    wip improving gpt architecture

--- small_gpt/models/gpt/gpt_model.py
+++ small_gpt/models/gpt/gpt_model.py
@@ -1,17 +1,35 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
+import inspect
 from .block import Block
 
 class GPT(nn.Module):
-    def __init__(self, vocabulary_size, n_embeddings, n_heads, context_length, n_layers, dropout):
+    def __init__(self, vocabulary_size, n_embeddings, n_heads, context_length, n_layers, dropout, bias):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocabulary_size, n_embeddings)
-        self.position_embedding_table = nn.Embedding(context_length, n_embeddings) # positions at which the tokens occur
-        self.blocks = nn.Sequential(*[Block(n_embeddings, n_heads, context_length, dropout) for _ in range(n_layers)])
-        self.layer_norm = nn.LayerNorm(n_embeddings)
-        self.lm_head = nn.Linear(n_embeddings, vocabulary_size)
+        
+        self.transformer = nn.ModuleDict(dict(
+            token_embedding_table = nn.Embedding(vocabulary_size, n_embeddings),
+            position_embedding_table = nn.Embedding(context_length, n_embeddings), # positions at which the tokens occur
+            drop = nn.Dropout(dropout),
+            blocks = nn.ModuleList([Block(n_embeddings, n_heads, context_length, dropout) for _ in range(n_layers)]),
+            layer_norm = nn.LayerNorm(n_embeddings, bias),
+        ))
+        self.lm_head = nn.Linear(n_embeddings, vocabulary_size, bias=False)
+        self.transformer.token_embedding_table.weight = self.lm_head.weight # weight tying
         self.apply(self._init_weights)
+        
+        # scaled initialization to the residual projections
+        for pn, p in self.named_parameters():
+            if pn.endswith('c_proj.weight'):
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * n_layers))
+                
+    def get_parameters_number(self, non_embedding=True):
+        n = sum(p.numel() for p in self.parameters())
+        if non_embedding:
+            n -= self.transformer.wpe.weight.numel()
+        return n
         
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
@@ -22,28 +40,73 @@ class GPT(nn.Module):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
         
     def forward(self, idx, targets=None):
-        device = "cuda" if torch.cuda.is_available() else "cpu"
+        ###
+        b, t = idx.size()
+        assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
+        pos = torch.arange(0, t, dtype=torch.long, device=device) # shape (t)
+
+        # forward the GPT model itself
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (b, t, n_embd)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (t, n_embd)
+        x = self.transformer.drop(tok_emb + pos_emb)
+        for block in self.transformer.h:
+            x = block(x)
+        x = self.transformer.ln_f(x)
+
+        if targets is not None:
+            # if we are given some desired targets also calculate the loss
+            logits = self.lm_head(x)
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+        else:
+            # inference-time mini-optimization: only forward the lm_head on the very last position
+            logits = self.lm_head(x[:, [-1], :]) # note: using list [-1] to preserve the time dim
+            loss = None
+
+        # return logits, loss
+        ###
+        
+        device = idx.device
         
         _, context_length = idx.shape
         
-        token_embedding = self.token_embedding_table(idx) # returns (batch, time, channel) = (batch_size, context_length, vocabulary_size)
-        position_embedding = self.position_embedding_table(torch.arange(context_length, device=device)) # (context_size, channel)
-        x = token_embedding + position_embedding # (batch_size, context_length, channel) - information for both tokens' identities and their positions                                                   
-        x = self.blocks(x)
+        token_embedding = self.transformer.token_embedding_table(idx) # returns (batch, time, channel) = (batch_size, context_length, vocabulary_size)
+        position_embedding = self.transformer.position_embedding_table(torch.arange(context_length, device=device)) # (context_size, channel)
+        x = self.transformer.drop(token_embedding + position_embedding) # (batch_size, context_length, channel) - information for both tokens' identities and their positions
+        
+        for block in self.transformer.blocks:
+            x = block(x)
+
         x = self.layer_norm(x)
-        logits = self.lm_head(x) # (batch, context_length, vocabulary_size)
         
         if targets is None:
+            logits = self.lm_head(x[:, [-1], :]) # only forward lm_head on the very last position
             loss = None
-        else:        
-            batch, time, channel = logits.shape
-            
-            logits = logits.view(batch * time, channel)
-            targets = targets.view(batch * time)
-            
-            loss = F.cross_entropy(logits, targets)
+        else:
+            logits = self.lm_head(x) # (batch, context_length, vocabulary_size)
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
         
         return logits, loss
+    
+    def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
+        param_dict = {pn: p for pn, p in self.named_parameters()} # all candidate parameters
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad} # remove parameters that don't require grad
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        no_decay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': no_decay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in no_decay_params)
+        print(f"Number of decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"Number of non-decayed parameter tensors: {len(no_decay_params)}, with {num_nodecay_params:,} parameters")
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == 'cuda'
+        extra_args = dict(fused=True) if use_fused else dict()
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
+        print(f"Using fused AdamW: {use_fused}")
+
+        return optimizer
     
     def generate(self, idx, max_new_tokens):
         _, context_length = idx.shape
