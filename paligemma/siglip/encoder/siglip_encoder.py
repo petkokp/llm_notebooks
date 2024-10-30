035f28d7e9955b89ec9151a5e99d42f32af37a78
Petko Petkov <petko.petkov987@gmail.com>
2024-10-30 22:15:06 +0200

    siglip encoder

---
+++ paligemma/siglip/encoder/siglip_encoder.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+from siglip.siglip_config import SiglipVisionConfig
+from siglip_encoder_layer import SiglipEncoderLayer
+
+class SiglipEncoder(nn.Module):
+    def __init__(self, config: SiglipVisionConfig):
+        super().__init__()
+        self.config = config
+        self.layers = nn.ModuleList(
+            [SiglipEncoderLayer(config) for _ in range(config.num_hidden_layers)]
+        )
+
+    def forward(
+        self,
+        inputs_embeds: torch.Tensor
+    ) -> torch.Tensor:
+        # inputs_embeds: [Batch_Size, Num_Patches, Embed_Dim]
+        hidden_states = inputs_embeds
+
+        for encoder_layer in self.layers:
+            # [Batch_Size, Num_Patches, Embed_Dim] -> [Batch_Size, Num_Patches, Embed_Dim]
+            hidden_states = encoder_layer(hidden_states)
+
+        return hidden_states
