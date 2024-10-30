d762b16f1673289d1a1c75a63cfd7fc2b81b0fff
Petko Petkov <petko.petkov987@gmail.com>
2024-10-30 22:24:38 +0200

    combined modules into a siglip vision transformer

---
+++ paligemma/siglip/siglip_transformer.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+from siglip_config import SiglipVisionConfig
+from siglip_embeddings import SiglipVisionEmbeddings
+from encoder.siglip_encoder import SiglipEncoder
+
+class SiglipVisionTransformer(nn.Module):
+    def __init__(self, config: SiglipVisionConfig):
+        super().__init__()
+        self.config = config
+        embed_dim = config.hidden_size
+
+        self.embeddings = SiglipVisionEmbeddings(config)
+        self.encoder = SiglipEncoder(config)
+        self.post_layernorm = nn.LayerNorm(embed_dim, eps=config.layer_norm_eps)
+
+    def forward(self, pixel_values: torch.Tensor) -> torch.Tensor:
+        # pixel_values: [Batch_Size, Channels, Height, Width] -> [Batch_Size, Num_Patches, Embed_Dim]
+        hidden_states = self.embeddings(pixel_values)
+
+        last_hidden_state = self.encoder(inputs_embeds=hidden_states)
+
+        last_hidden_state = self.post_layernorm(last_hidden_state)
+
+        return last_hidden_state
