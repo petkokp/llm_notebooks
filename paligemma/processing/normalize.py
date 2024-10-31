c6b67102f20f09119665c19084e03c5fe5d76c14
Petko Petkov <petko.petkov987@gmail.com>
2024-10-31 16:36:13 +0200

    normalize helper function

---
+++ paligemma/processing/normalize.py
@@ -0,0 +1,12 @@
+import numpy as np
+from typing import Union, Iterable
+
+def normalize(
+    image: np.ndarray,
+    mean: Union[float, Iterable[float]],
+    std: Union[float, Iterable[float]],
+) -> np.ndarray:
+    mean = np.array(mean, dtype=image.dtype)
+    std = np.array(std, dtype=image.dtype)
+    image = (image - mean) / std
+    return image
