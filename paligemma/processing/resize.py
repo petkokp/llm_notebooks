451793c8ff7b1a40577d9aafe01c2b47d497ac6f
Petko Petkov <petko.petkov987@gmail.com>
2024-10-31 16:28:19 +0200

    implemented resize helper function

---
+++ paligemma/processing/resize.py
@@ -0,0 +1,15 @@
+import numpy as np
+from PIL import Image
+from typing import Tuple, Optional
+
+def resize(
+    image: Image,
+    size: Tuple[int, int],
+    resample: Image.Resampling = None,
+    reducing_gap: Optional[int] = None,
+) -> np.ndarray:
+    height, width = size
+    resized_image = image.resize(
+        (width, height), resample=resample, reducing_gap=reducing_gap
+    )
+    return resized_image
