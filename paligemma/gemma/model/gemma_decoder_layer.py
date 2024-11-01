447db3f252127f25015147cb834d3f599c05c993
Petko Petkov <petko.petkov987@gmail.com>
2024-11-01 11:49:52 +0200

    implemented gemma decoder layer

---
+++ paligemma/gemma/model/gemma_decoder_layer.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+from typing import Optional, Tuple
+from kv_cache import KVCache
+from config.gemma_config import GemmaConfig
+from attention.gemma_attention import GemmaAttention
+from gemma_mlp import GemmaMLP
+from gemma_rms_norm import GemmaRMSNorm
+
+class GemmaDecoderLayer(nn.Module):
+    def __init__(self, config: GemmaConfig, layer_idx: int):
+        super().__init__()
+        self.hidden_size = config.hidden_size
+
+        self.self_attn = GemmaAttention(config=config, layer_idx=layer_idx)
+
+        self.mlp = GemmaMLP(config)
+        self.input_layernorm = GemmaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.post_attention_layernorm = GemmaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        kv_cache: Optional[KVCache] = None,
+    ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
+        residual = hidden_states
+        # [Batch_Size, Seq_Len, Hidden_Size]
+        hidden_states = self.input_layernorm(hidden_states)
+
+        # [Batch_Size, Seq_Len, Hidden_Size]
+        hidden_states, _, = self.self_attn(
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            kv_cache=kv_cache,
+        )
+        # [Batch_Size, Seq_Len, Hidden_Size]
+        hidden_states = residual + hidden_states
+
+        # [Batch_Size, Seq_Len, Hidden_Size]
+        residual = hidden_states
+        # [Batch_Size, Seq_Len, Hidden_Size]
+        hidden_states = self.post_attention_layernorm(hidden_states)
+        # [Batch_Size, Seq_Len, Hidden_Size]
+        hidden_states = self.mlp(hidden_states)
+        # [Batch_Size, Seq_Len, Hidden_Size]
+        hidden_states = residual + hidden_states
+
+        return hidden_states
