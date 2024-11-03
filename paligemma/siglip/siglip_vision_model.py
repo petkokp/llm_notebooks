958fa00536dd0d451311db4f2e52892b7403b7cf
Petko Petkov <petko.petkov987@gmail.com>
2024-11-03 21:13:15 +0200

    siglip vision model wrapper

---
+++ paligemma/siglip/siglip_vision_model.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+from typing import Tuple
+from siglip_config import SiglipVisionConfig
+from siglip_transformer import SiglipVisionTransformer
+
+class SiglipVisionModel(nn.Module):
+    def __init__(self, config: SiglipVisionConfig):
+        super().__init__()
+        self.config = config
+        self.vision_model = SiglipVisionTransformer(config)
+
+    def forward(self, pixel_values) -> Tuple:
+        # [Batch_Size, Channels, Height, Width] -> [Batch_Size, Num_Patches, Embed_Dim]
+        return self.vision_model(pixel_values=pixel_values) 
