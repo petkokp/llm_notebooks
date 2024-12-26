c9453e93aa38b0fa9a84150a259a2a00e5f3bec8
Petko Petkov <petko.petkov987@gmail.com>
2024-12-26 13:15:31 +0200

    added mmlu translation

---
+++ llama_bulgarian/dataset_translations/translate_mmlu.py
@@ -0,0 +1,13 @@
+from translate_dataset import translate_dataset
+
+dataset = {
+    "url": "cais/mmlu",
+    "config": "all"
+}
+
+translation_model = "Helsinki-NLP/opus-mt-tc-big-en-bg"
+output_dataset_url = "petkopetkov/mmlu-bg"
+
+exclude_columns = ["subject", "answer"]
+
+translate_dataset(dataset, translation_model, output_dataset_url, batch_size=32)
