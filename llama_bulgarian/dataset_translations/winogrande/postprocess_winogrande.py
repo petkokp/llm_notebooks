62470913e3d26e314cc11c02843d204e5ae2e9fa
Petko Petkov <petko.petkov987@gmail.com>
2024-12-27 20:23:51 +0200

    convert winogrande answers to strings and postprocess arc choices

--- llama_bulgarian/dataset_translations/winogrande/postprocess_winogrande.py
+++ llama_bulgarian/dataset_translations/winogrande/postprocess_winogrande.py
@@ -1,3 +1,5 @@
+from datasets import Value
+
 def postprocess_sentence(sentence):
     return sentence.replace(" (това е само пример) ", " _ ") # replace the bulgarian expression to get the underscore back in the sentence
 
@@ -5,7 +7,7 @@ def postprocess_winogrande_sentences(dataset, split):
     dataset[split] = dataset[split].map(lambda example: {
         'sentence': postprocess_sentence(example['sentence'])
     })
-    
+    dataset[split] = dataset[split].cast_column("answer", Value("string"))
     print(f"Processed {split} dataset")
 
     return dataset
