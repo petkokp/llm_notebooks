ee3f3174f0da3e4eaffb208de50a79a171ecc44b
Petko Petkov <petko.petkov987@gmail.com>
2024-12-27 12:52:11 +0200

    added arc challenge translation script

--- llama_bulgarian/dataset_translations/arc/postprocess_arc.py
+++ llama_bulgarian/dataset_translations/arc/postprocess_arc.py
@@ -5,7 +5,7 @@ def combine_choice_columns(example):
         }
         return example
 
-def postprocess_arceasy_choices(dataset, split):
+def postprocess_arc_choices(dataset, split):
     dataset[split] = dataset[split].map(combine_choice_columns)
     dataset[split] = dataset[split].remove_columns(['choices.text', 'choices.label'])
     print(f"Processed {split} dataset")
--- llama_bulgarian/dataset_translations/arc/preprocess_arc.py
+++ llama_bulgarian/dataset_translations/arc/preprocess_arc.py
@@ -1,4 +1,4 @@
-def preprocess_arceasy_choices(dataset, split):
+def preprocess_arc_choices(dataset, split):
     flattened = dataset.flatten() # splits "choices" into two columns - "choices.text" and "choices.label" to allow easier translation of the text
     print(f"Processed {split} dataset")
     return flattened
