28a1c9db62e4c1e999909e0272fc16718cfb30fd
Petko Petkov <petko.petkov987@gmail.com>
2025-01-15 15:58:49 +0100

    added push unsloth model script

---
+++ bulgarian_llms/training/push_model.py
@@ -0,0 +1,14 @@
+from unsloth import FastLanguageModel
+
+output = "smollm2-360M_finetuned_model"
+
+REPO_PATH = "petkopetkov/SmolLM2-360M-bg"
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name=output,
+    max_seq_length=2048,
+    dtype=None,
+    load_in_4bit=True,
+)
+
+model.push_to_hub_merged(REPO_PATH, tokenizer, save_method = "merged_16bit")
